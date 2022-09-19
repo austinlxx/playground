6a9a3302d87887e2638cd44b08f3fdba8f9b0637
Austin Lee <austin.dh.lee@gmail.com>
2022-09-19 10:49:22 -0700

    add api example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
+
 function App() {
+	const [joke, setJoke] = useState("");
+
+	useEffect(() => {
+		fetch("https://icanhazdadjoke.com/", {
+			method: "get",
+			headers: { Accept: "application/json" },
+		})
+			.then((res) => res.json())
+			.then((data) => setJoke(data.joke));
+	}, []);
+
 	return (
 		<div className={"font-body grid grid-cols-[1fr_900px_1fr] py-24"}>
 			<div />
 			<div>
 				<h1 className={"font-title text-4xl"}>Hello! Welcome to the playground.</h1>
-				<p className={"text-lg"}>Lets get started...</p>
+				<p className={"text-lg"}>Here's a dad joke (:</p>
+				<p className={"text-md py-8"}>{joke}</p>
 			</div>
 			<div />
 		</div>
