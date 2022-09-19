d9da1fe1a83dd90f659db130d32297a5bb0a646d
Austin Lee <austin.dh.lee@gmail.com>
2022-09-19 10:38:15 -0700

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import "./App.css";
-
 function App() {
-  return (
-    <div className={"font-body grid grid-cols-[1fr_900px_1fr] py-24"}>
-      <div/>
-      <div>
-        <h1 className={"font-title text-4xl"}>
-          Hello! Welcome to the playground.
-        </h1>
-        <p className={"text-lg"}>
-          Lets get started...
-        </p>
-      </div>
-      <div/>
-    </div>
-  );
+	return (
+		<div className={"font-body grid grid-cols-[1fr_900px_1fr] py-24"}>
+			<div />
+			<div>
+				<h1 className={"font-title text-4xl"}>Hello! Welcome to the playground.</h1>
+				<p className={"text-lg"}>Lets get started...</p>
+			</div>
+			<div />
+		</div>
+	);
 }
 
 export default App;
