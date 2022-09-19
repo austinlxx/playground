4cf4ba6624d073346528fd3ce7970961c15b8488
Austin Lee <austin@sendwyre.com>
2022-09-19 10:35:19 -0700

    minor fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./styles/global.css";
 import "./assets/fonts/FKGrotesk/font.css";
 import "./assets/fonts/FKGroteskMono/font.css";
 import "./assets/fonts/Gustavo/font.css";
 import "./assets/fonts/ArthemysDisplay/font.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
